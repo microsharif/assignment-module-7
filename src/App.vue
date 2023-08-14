3f110e5d00f733c20c32c275bd437902e2c87454
microsharif <72636135+microsharif@users.noreply.github.com>
2023-08-14 20:52:50 +0600

    upload project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Carousel from './components/Carousel.vue'
+import PieChart from './components/PieChart.vue'
+</script>
+
+<template>
+    <PieChart></PieChart>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/PieChart.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { onBeforeUnmount, onMounted } from 'vue'
+
+let chart = null
+
+const dataset = [
+  300, 50, 100
+]
+
+const data = {
+  labels: [
+    'Red',
+    'Blue',
+    'Yellow'
+  ],
+  datasets: [{
+    label: 'My First Dataset',
+    data: dataset,
+    backgroundColor: [
+      'rgb(255, 99, 132)',
+      'rgb(54, 162, 235)',
+      'rgb(255, 205, 86)',
+      'rgb(43, 105, 86)',
+      'rgb(21, 21, 186)',
+    ],
+    hoverOffset: 4
+  }]
+}
+
+const config = {
+  type: 'pie',
+  data: data,
+}
+
+onMounted(() => {
+  const ctx = document.getElementById('chart')
+  chart = new Chart(ctx, config)
+})
+
+onBeforeUnmount(() => {
+    chart.destroy()
+})
+
+</script>
+
+<template>
+  <div class=" mx-auto w-[400px] h-[400px] bg-gray-400">
+    <canvas id="chart">
+    </canvas>
+  </div>
+</template>
+
+<style scoped></style>
